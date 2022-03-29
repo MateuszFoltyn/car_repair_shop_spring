8bd718fe695fbc7552aaa938538b9599eb04959e
mient <przemekdziubak@wp.pl>
2022-03-29 20:11:50 +0200

    RestCarService, Pusty CarRestController

--- car_repair_shop/src/main/java/com/example/service/RestCarService.java
+++ car_repair_shop/src/main/java/com/example/service/RestCarService.java
@@ -1,6 +1,39 @@
 package com.example.service;
 
+
+import com.example.web.RestCar;
+import com.example.web.RestCarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class RestCarService {
 
+    private final RestCarRepository restCarRepository;
+
+    public RestCar add(RestCar restCar) {
+        return restCarRepository.add(restCar);
+    }
+
+    public List<RestCar> findAll() {
+        return restCarRepository.findAll();
+    }
+
+    public List<RestCar> findByMark(String mark) {
+        return restCarRepository.findByMark(mark);
+    }
+
+    public RestCar findById(Long id) {
+        return restCarRepository.findById(id);
+    }
+
+    public boolean deleteById(Long id) {
+        return restCarRepository.deleteById(id);
+    }
 
 }
---
+++ car_repair_shop/src/main/java/com/example/web/CarRestController.java
@@ -0,0 +1,12 @@
+package com.example.web;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping()   //TODO UZUPELNIC NAWIAS PATH'EM !!!!!!!!
+public class CarRestController {
+}
