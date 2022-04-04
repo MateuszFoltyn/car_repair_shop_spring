cdbe5fd4eb1821a229977ad9c044688ca9a40ac6
mient <przemekdziubak@wp.pl>
2022-04-04 19:49:11 +0200

    Dodanie do CarController metody findByCustomerId Dodanie do CustomerService metody findByBrand

--- car_repair_shop/src/main/java/com/example/car_repair_shop/service/CustomerService.java
+++ car_repair_shop/src/main/java/com/example/car_repair_shop/service/CustomerService.java
@@ -1,17 +1,21 @@
 package com.example.car_repair_shop.service;
 
+import com.example.car_repair_shop.persistance.Car;
 import com.example.car_repair_shop.persistance.Customer;
+import com.example.car_repair_shop.repository.CarRepository;
 import com.example.car_repair_shop.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CarRepository carRepository;
 
     public Customer save(Customer customer) {
         return customerRepository.save(customer);
@@ -26,7 +30,11 @@ public class CustomerService {
     }
 
     public List<Customer> findByBrand(String brand) {
-        return customerRepository.
+        return carRepository.findByBrand(brand).stream()
+                .map(Car::getCustomer)
+                .collect(Collectors.toList());
+
+//        return customerRepository.findAllByCarBrand(brand);
     }
 
     public List<Customer> findBySecondName(String secondName) {
